620c336c0543b921a42465736ad3acb414ba2ed9
vinic87s <vinicius.ferrari@medcof.tech>
2026-01-08 15:42:30 +0000

    alteração texto index e identações com o prettier

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 function Home() {
-  return (
-    <h1>
-      {" "}
-      Oi gab, um beijo do seu garoto de programa favorito! estou com saudades xd
-    </h1>
-  );
+  return <h1> teste 2.0</h1>;
+}
+function teste() {
+  console.log("teste");
 }
 
 export default Home;
