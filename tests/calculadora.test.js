b4d4c0d8e06267ac21274178c0d520638606f096
vinic87s <vinicius.ferrari@medcof.tech>
2026-01-09 19:59:12 +0000

    adiciona calculadora do models(funcional) e desativa com comments (//) os testes inciais da issue

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -1,16 +1,24 @@
-test("primeiro teste", callbackFunction);
-function callbackFunction() {
-  console.log("retorna essa mensagem");
-}
+// test("primeiro teste", callbackFunction);
+// function callbackFunction() {
+//   console.log("retorna essa mensagem");
+// }
 
-test("nsegundo teste", function () {
-  console.log("e esse?");
-});
+// test("nsegundo teste", function () {
+//   console.log("e esse?");
+// });
 
-test("teste 3", () => {
-  console.log("terceiro teste??!");
-});
+// test("teste 3", () => {
+//   console.log("terceiro teste??!");
+// });
 
-test("espero que 1 seja 1", () => {
-  expect(1).toBe(1);
+// test("espero que 1 seja 1", () => {
+//   expect(1).toBe(1);
+// });
+
+const calculadora = require("../models/calculadora.js");
+
+test("somar 4 + 2 deveria retornar 6", () => {
+  const resultado = calculadora.somar(2, 4);
+  console.log(resultado);
+  expect(resultado).toBe(6);
 });
