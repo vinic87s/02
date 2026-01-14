da1c9aa6c81199a5d3b0e76161b5beba518363e1
vinic87s <vinicius.ferrari@medcof.tech>
2026-01-14 14:19:24 +0000

    variavel de ambiente no db

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,10 @@
-function status(request, response) {
-  response.status(200).json({ chave: "teste de status ÇÇÇÇ hmm" });
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;")
+  console.log(database);
+  console.log(result);
+  response.status(200).json({ chave: "teste zde status ÇÇÇÇ hmm" });
 }
 
 export default status;
